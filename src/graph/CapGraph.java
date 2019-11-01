6fc01079425e73a7214f0915ea0c2611f36538f3
SEVEN-WORK <SEVEN-WORK@192.168.10.88>
2019-11-01 14:30:54 +0200

    first commit

---
+++ src/graph/CapGraph.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import util.GraphLoader;
+
+/**
+ * @author Your name here.
+ * 
+ * For the warm up assignment, you must implement your Graph in a class
+ * named CapGraph.  Here is the stub file.
+ *
+ */
+public class CapGraph implements Graph {
+
+	private HashMap<Integer, HashSet<Integer>> nodes = new HashMap<>();
+	
+	
+	/* (non-Javadoc)
+	 * @see graph.Graph#addVertex(int)
+	 */
+	@Override
+	public void addVertex(int num) {
+		// TODO Auto-generated method stub
+		if (!nodes.containsKey(num)) {
+			nodes.put(num, new HashSet<Integer>());
+			
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#addEdge(int, int)
+	 */
+	@Override
+	public void addEdge(int from, int to) {
+		// TODO Auto-generated method stub
+		
+		if (nodes.containsKey(from)) {
+			nodes.get(from).add(to);
+		}
+		if (!nodes.containsKey(to)) {
+			nodes.put(to, new HashSet<Integer>());
+			nodes.get(to).add(from);
+			
+			
+		}
+		
+		
+
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#getEgonet(int)
+	 */
+	@Override
+	public Graph getEgonet(int center) {
+		// TODO Auto-generated method stub
+		CapGraph graph = new CapGraph();
+		graph.nodes.put(center, nodes.get(center));
+		//System.out.println(graph.nodes.size());
+		
+		return graph;
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#getSCCs()
+	 */
+	@Override
+	public List<Graph> getSCCs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see graph.Graph#exportGraph()
+	 */
+	@Override
+	public HashMap<Integer, HashSet<Integer>> exportGraph() {
+		// TODO Auto-generated method stub
+		return nodes;
+	}
+
+	
+	public static void main(String[] args) {
+		/*CapGraph graph = new CapGraph();
+		graph.addVertex(7);
+		graph.addVertex(8);
+		graph.addVertex(9);
+		graph.addVertex(6);
+		graph.addVertex(5);
+		graph.addEdge(7, 8);
+		graph.addEdge(7, 9);
+		
+		System.out.println(graph.getEgonet(7).exportGraph().get(7).size());
+		*/
+		Graph graph = new CapGraph();
+        GraphLoader.loadGraph(graph, "data/facebook_ucsd.txt");
+        HashMap<Integer, HashSet<Integer>> res = graph.getEgonet(0).exportGraph();
+		System.out.println(res.get(0));
+		
+		
+		
+		
+	}
+
+	
+}
